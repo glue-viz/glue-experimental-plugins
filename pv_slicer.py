70b68cf3cd17cabaadc9d47d9db12c9c4e3ed07b
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-02-21 20:48:36 +0000

    Minor fixes

--- pv_slicer.py
+++ pv_slicer.py
@@ -7,12 +7,12 @@
 # - Create a class that inherits from PathTool to make a tool that does
 #   something after drawing a path.
 #
-# - Use the ``on_end`` method to describe what happens when the user presses
+# - Use the ``on_finish`` method to describe what happens when the user presses
 #   'enter' after drawing the path. Note that other standard methods could be
 #   defined, such as ``on_start``, ``on_draw``, etc.
 #
 # - The main widget that the tool is attached to is available as
-#    ``self.widget``, and the user would need to know how to access the data
+#    ``self.parent_widget``, and the user would need to know how to access the data
 #    from that widget.
 #
 # - The user can use add_cursor_callback method on widgets to add standard
@@ -21,7 +21,7 @@
 # These can all be easily described in docs describing how to define tools.
 
 from glue.tools import PathTool
-from glue.widgets import ImageWidget
+from glue.parent_widgets import ImageWidget
 from pvextractor import pv_slicer
 
 
@@ -29,19 +29,18 @@ class PVSlicingTool(PathTool):
 
     def on_finish(self):
 
-        # Extract vertices from path
-        x, y = self.path
+        # Path vertices are available as self.x and self.y
 
         # Compute PV slice. Tools have a property widget that returns the
         # widget to which they are attached. This means that to access the data
-        # we use self.widget.data.
-        pv_slice, x_slice, y_slice, wcs = pv_slicer(self.widget.data, x, y)
+        # we use self.parent_widget.data.
+        pv_slice, x_slice, y_slice, wcs = pv_slicer(self.parent_widget.data, self.x, self.y)
 
         # We now want to initialize a new viewer that will show the PV slice
-        if not hasattr(self.viewer):
-            self.viewer = ImageWidget(pv_slice, wcs=wcs)
+        if not hasattr(self.slice_viewer):
+            self.slice_viewer = ImageWidget(pv_slice, wcs=wcs)
         else:
-            self.viewer.set_data(pv_slice, wcs=wcs)
+            self.slice_viewer.set_data(pv_slice, wcs=wcs)
 
         self.pv_slice = pv_slice
         self.x_slice = x_slice
@@ -54,7 +53,7 @@ class PVSlicingTool(PathTool):
 
         # Crosshairs could include either a cross or a full crosshair (or
         # different types of symbols)
-        self.viewer.add_cursor_callback(self.update_image_crosshair)
+        self.slice_viewer.add_cursor_callback(self.update_image_crosshair)
 
     def cursor_callback(self, xdata, ydata):
 
@@ -73,11 +72,11 @@ class PVSlicingTool(PathTool):
         # coordinate in the PV slice.
         z = ydata
 
-        # Update crosshairs in main image
-        self.widget.move_crosshair(x, y)
+        # Update crosshairs in parent widget
+        self.parent_widget.move_crosshair(x, y)
 
-        # Also update slice
-        self.widget.set_slice(int(z))
+        # Also update slice in parent widget
+        self.parent_widget.set_slice(int(z))
 
 
 tool_registry.add(PVSlicingTool, widget_cls=ImageWidget)
