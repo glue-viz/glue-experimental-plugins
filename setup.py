73c50ccb1d1503d4fa43e8c97a91c0d21ef79b23
Thomas Robitaille <thomas.robitaille@gmail.com>
2015-03-29 21:28:17 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+from setuptools import setup, Command, find_packages
+from setuptools.command.test import test as TestCommand
+
+import os
+import sys
+import subprocess
+
+
+setup(name='glue_exp',
+      version="0.1.dev0",
+      description = "Experimental plugins for glue",
+      author='Thomas Robitaille, Chris Beaumont',
+      author_email='glueviz@gmail.com',
+      url='http://glueviz.org',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'License :: OSI Approved :: BSD License'
+          ],
+      packages = find_packages(),
+      package_data={'': ['*.ui']}
+    )
