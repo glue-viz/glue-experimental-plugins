c0a75ade44ba35b640f49bc52f608f5eee5979e7
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-08-30 12:00:32 +0100

    Fix compatibility with new toolbar API in glue>=0.9

--- glue_exp/tools/floodfill_selection/floodfill_selection.py
+++ glue_exp/tools/floodfill_selection/floodfill_selection.py
@@ -2,16 +2,18 @@ import os
 import numpy as np
 
 from glue.viewers.common.qt.mouse_mode import MouseMode
-
-try:
-    from glue.external.qt import QtGui
-except ImportError:
-    from qtpy import QtGui
+from glue.core.edit_subset_mode import EditSubsetMode
+from glue.core.subset import MaskSubsetState
 
 from .floodfill_scipy import floodfill_scipy
 
-from glue.core.edit_subset_mode import EditSubsetMode
-from glue.core.subset import MaskSubsetState
+try:
+    from glue.config import viewer_tool
+    GLUE_LT_09 = False
+except ImportError:  # Compatibility with glue < 0.9
+    def viewer_tool(x):
+        return x
+    GLUE_LT_09 = True
 
 __all__ = ['FloodfillSelectionTool']
 
@@ -20,34 +22,75 @@ ROOT = os.path.dirname(__file__)
 WARN_THRESH = 1000000000000  # warn when floodfilling large images
 
 
-class FloodfillSelectionTool(object):
+@viewer_tool
+class FloodfillSelectionTool(MouseMode):
+    """
+    Creates selection by using the mouse to pick regions using the flood fill
+    algorithm: https://en.wikipedia.org/wiki/Flood_fill
+    """
 
-    def __init__(self, widget=None):
-        self.widget = widget
-        self.data_object = None
+    icon = os.path.join(ROOT, "glue_floodfill.png")
+    tool_id = 'Flood fill'
+    action_text = 'Flood fill'
+    tool_tip = ('Define a region of interest with the flood fill '
+                'algorithm. Click to define the starting pixel and '
+                'drag (keeping the mouse clicked) to grow the '
+                'selection.')
 
-    def _get_modes(self, axes):
-        self._mode = FloodfillMode(axes,
-                                   move_callback=self._floodfill_roi,
-                                   release_callback=self._floodfill_roi)
-        return [self._mode]
+    def __init__(self, *args, **kwargs):
 
-    # @set_cursor(Qt.WaitCursor)
+        super(FloodfillSelectionTool, self).__init__(*args, **kwargs)
+
+        self._start_event = None
+        self._end_event = None
+
+        self._move_callback = self._floodfill_roi
+        self._release_callback = self._floodfill_roi
+
+        if GLUE_LT_09:
+            try:
+                from glue.external.qt import QtGui
+            except:
+                from qtpy import QtGui
+            self.icon = QtGui.QIcon(os.path.join(ROOT, "glue_floodfill.png"))
+            self.mode_id = 'Flood fill'
+            self.action_text = 'Flood fill'
+            self.tool_tip = ('Define a region of interest with the flood fill '
+                             'algorithm. Click to define the starting pixel and '
+                             'drag (keeping the mouse clicked) to grow the '
+                             'selection.')
+            self.viewer = args[0]
+
+
+    def press(self, event):
+        self._start_event = event
+        super(FloodfillSelectionTool, self).press(event)
+
+    def move(self, event):
+        self._end_event = event
+        super(FloodfillSelectionTool, self).move(event)
+
+    def release(self, event):
+        self._end_event = event
+        super(FloodfillSelectionTool, self).release(event)
+        self._start_event = None
+        self._end_event = None
+
     def _floodfill_roi(self, mode):
         """
-        Callback for FloodfillMode.
+        Callback for FloodfillSelectionTool.
         """
 
         if mode._start_event is None or mode._end_event is None:
             return
 
-        data = self.widget.client.display_data
-        att = self.widget.client.display_attribute
+        data = self.viewer.client.display_data
+        att = self.viewer.client.display_attribute
 
         if data is None or att is None:
             return
 
-        if data.size > WARN_THRESH and not self.widget._confirm_large_image(data):
+        if data.size > WARN_THRESH and not self.viewer._confirm_large_image(data):
             return
 
         # Determine length of dragging action in units relative to the figure
@@ -63,7 +106,7 @@ class FloodfillSelectionTool(object):
         if data.ndim == 2:
             start_coord = (y, x)
         else:
-            z = int(round(self.widget.client.slice[self.profile_axis]))
+            z = int(round(self.viewer.client.slice[self.profile_axis]))
             start_coord = (z, y, x)
 
         # We convert the length in relative figure units to a threshold - we make
@@ -83,50 +126,21 @@ class FloodfillSelectionTool(object):
 
     @property
     def profile_axis(self):
-        slc = self.widget.client.slice
+        slc = self.viewer.client.slice
         candidates = [i for i, s in enumerate(slc) if s not in ['x', 'y']]
         if len(candidates) == 0:
             return None
         else:
-            return max(candidates, key=lambda i: self.widget.client.display_data.shape[i])
+            return max(candidates, key=lambda i: self.viewer.client.display_data.shape[i])
+
+    # TODO: the following three methods are for backward-compatibility with 
+    #       glue < 0.9, and can be removed once we support only glue >= 0.9.
 
     def _display_data_hook(self, data):
         pass
 
+    def _get_modes(self, axes):
+        return [self]
+
     def close(self):
         pass
-
-
-class FloodfillMode(MouseMode):
-    """
-    Creates selection by using the mouse to pick regions using the flood fill
-    algorithm: https://en.wikipedia.org/wiki/Flood_fill
-    """
-
-    def __init__(self, *args, **kwargs):
-
-        super(FloodfillMode, self).__init__(*args, **kwargs)
-
-        self.icon = QtGui.QIcon(os.path.join(ROOT, "glue_floodfill.png"))
-        self.mode_id = 'Flood fill'
-        self.action_text = 'Flood fill'
-        self.tool_tip = ('Define a region of interest with the flood fill '
-                         'algorithm. Click to define the starting pixel and '
-                         'drag (keeping the mouse clicked) to grow the '
-                         'selection.')
-        self._start_event = None
-        self._end_event = None
-
-    def press(self, event):
-        self._start_event = event
-        super(FloodfillMode, self).press(event)
-
-    def move(self, event):
-        self._end_event = event
-        super(FloodfillMode, self).move(event)
-
-    def release(self, event):
-        self._end_event = event
-        super(FloodfillMode, self).release(event)
-        self._start_event = None
-        self._end_event = None
