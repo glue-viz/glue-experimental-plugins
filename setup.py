3d098c646352aa1824fda26d42798135e7851d8f
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-07-07 20:34:27 +0100

    Update package layout to use setup.cfg and setuptools_scm

--- setup.py
+++ setup.py
@@ -1,35 +1,16 @@
 #!/usr/bin/env python
-
 from __future__ import print_function
 
-from setuptools import setup, find_packages
+import sys
+from distutils.version import LooseVersion
 
-entry_points = """
-[glue.plugins]
-webcam_importer=glue_exp.importers.webcam:setup
-vizier_importer=glue_exp.importers.vizier:setup
-contour_selection=glue_exp.tools.contour_selection:setup
-floodfill_selection=glue_exp.tools.floodfill_selection:setup
-zoom_buttons=glue_exp.tools.zoom_buttons:setup
-"""
+try:
+    import setuptools
+    assert LooseVersion(setuptools.__version__) >= LooseVersion('30.3')
+except (ImportError, AssertionError):
+    sys.stderr.write("ERROR: setuptools 30.3 or later is required\n")
+    sys.exit(1)
 
-setup(name='glue_exp',
-      version="0.1.dev0",
-      description = "Experimental plugins for glue",
-      author='Thomas Robitaille, Chris Beaumont',
-      author_email='glueviz@gmail.com',
-      url='http://glueviz.org',
-      classifiers=[
-          'Intended Audience :: Science/Research',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Topic :: Scientific/Engineering :: Visualization',
-          'License :: OSI Approved :: BSD License'
-          ],
-      packages = find_packages(),
-      package_data={'glue_exp.importers.vizier': ['*.ui'], 'glue_exp.importers.webcam':['*.ui'], '': ['*.png']},
-      entry_points=entry_points
-    )
+from setuptools import setup
+
+setup(use_scm_version=True)
